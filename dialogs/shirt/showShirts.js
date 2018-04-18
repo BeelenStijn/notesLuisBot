b6be446093f730e4d3870d68a58aab26119586af
BeelenStijn <38423636+BeelenStijn@users.noreply.github.com>
2018-04-18 10:28:37 +0200

    few modifications

--- dialogs/shirt/showShirts.js
+++ dialogs/shirt/showShirts.js
@@ -21,5 +21,78 @@ module.exports = function(session) {
                 builder.CardAction.imBack(session, "buy classic gray t-shirt", "Buy")
             ])
     ]);
+    // format for adaptive cards
+    /*msg.addAttachment({
+        contentType: "application/vnd.microsoft.card.adaptive",
+        content: {
+            type: "AdaptiveCard",
+            speak: "<s>Your  meeting about \"Adaptive Card design session\"<break strength='weak'/> is starting at 12:30pm</s><s>Do you want to snooze <break strength='weak'/> or do you want to send a late notification to the attendees?</s>",
+               body: [
+                    {
+                        "type": "TextBlock",
+                        "text": "Adaptive Card design session",
+                        "size": "large",
+                        "weight": "bolder"
+                    },
+                    {
+                        "type": "TextBlock",
+                        "text": "Conf Room 112/3377 (10)"
+                    },
+                    {
+                        "type": "TextBlock",
+                        "text": "12:30 PM - 1:30 PM"
+                    },
+                    {
+                        "type": "TextBlock",
+                        "text": "Snooze for"
+                    },
+                    {
+                        "type": "Input.ChoiceSet",
+                        "id": "snooze",
+                        "style":"compact",
+                        "choices": [
+                            {
+                                "title": "5 minutes",
+                                "value": "5",
+                                "isSelected": true
+                            },
+                            {
+                                "title": "15 minutes",
+                                "value": "15"
+                            },
+                            {
+                                "title": "30 minutes",
+                                "value": "30"
+                            }
+                        ]
+                    }
+                ],
+                "actions": [
+                    {
+                        "type": "Action.OpenUrl",
+                        "method": "POST",
+                        "url": "http://foo.com",
+                        "title": "Snooze"
+                    },
+                    {
+                        "type": "Action.OpenUrl",
+                        "method": "POST",
+                        "url": "http://foo.com",
+                        "title": "I'll be late"
+                    },
+                    {
+                        "type": "Action.OpenUrl",
+                        "method": "POST",
+                        "url": "http://foo.com",
+                        "title": "Dismiss"
+                    }
+                ]
+        }
+    });*/
+    /*builder.Prompts.text(session, 'Are you sure that you want to cancel this transaction?', {            
+        speak: 'Are you <emphasis level=\"moderate\">sure</emphasis> that you want to cancel this transaction?',
+        retrySpeak: 'Are you <emphasis level=\"moderate\">sure</emphasis> that you want to cancel this transaction?',
+        inputHint: builder.InputHint.expectingInput
+    });*/
     session.send(msg).endDialog();
 }
